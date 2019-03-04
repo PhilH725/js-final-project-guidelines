a2a8928621567c6b3ebc7de20da25ccd27968a94
shannon-nabors <shannon.nabors@gmail.com>
2019-03-04 17:23:45 -0500

    made territory clickable to toggle active status

--- src/index.js
+++ src/index.js
@@ -15,13 +15,37 @@ function init() {
   getTerritories()
   endButton().addEventListener('click', endTurn)
   setTextBox()
+  getBoard().addEventListener('click', handleBoardClick)
+}
+
+function handleBoardClick(e) {
+  if (e.target && e.target.className === "territory") {
+    let ter = Territory.find(e.target.id.slice(10))
+    if (ter.player_id == turn) {
+      if (!Territory.anyActiveTerritories() || ter.active == true) {
+        selectTerritory(ter)
+      }
+    }
+  } else if (e.target && e.target.nodeName === "BUTTON"){
+    let ter = Territory.find(e.target.parentElement.id.slice(10))
+    let power = powerBar(ter).textContent.slice(7)
+
+    if (e.target.textContent === "-" && power > 0) {
+      alterPower(ter, -1)
+    } else if (e.target.textContent === "+" && powerStore > 0) {
+      alterPower(ter, 1)
+    }
+  }
 }
 
 // fetch territories from db
 function getTerritories() {
   return fetch('http://localhost:3000/territories')
   .then(res => res.json())
-  .then(json => renderGameBoard(json))
+  .then(territoryData => {
+    Territory.createTerritoriesFromDB(territoryData)
+    renderGameBoard(territoryData)
+  })
 }
 
 // iterate through territories to create game board
@@ -48,33 +72,21 @@ function renderTerritory(ter) {
   powerEl.innerText = `Power: ${ter.power} `
 
   if (ter.player_id === turn) {
-  minusBtn = document.createElement('button')
-  terDiv.appendChild(minusBtn)
-  minusBtn.innerText = '-'
+    minusBtn = document.createElement('button')
+    terDiv.appendChild(minusBtn)
+    minusBtn.innerText = '-'
 
-  plusBtn = document.createElement('button')
-  terDiv.appendChild(plusBtn)
-  plusBtn.innerText = '+'
+    plusBtn = document.createElement('button')
+    terDiv.appendChild(plusBtn)
+    plusBtn.innerText = '+'
 
-  terDiv.appendChild(document.createElement('br'))
+    terDiv.appendChild(document.createElement('br'))
 
-  activeBtn = document.createElement('button')
-  terDiv.appendChild(activeBtn)
-  activeBtn.innerText = 'Choose Territory'
+    // activeBtn = document.createElement('button')
+    // terDiv.appendChild(activeBtn)
+    // activeBtn.innerText = 'Choose Territory'
 
-  minusBtn.addEventListener('click', ()=> {
-    if (ter.power > 0) {
-      alterPower(ter, -1)
-    }
-  })
-
-  plusBtn.addEventListener('click', ()=> {
-    if (powerStore > 0) {
-      alterPower(ter, 1)
-    }
-  })
-
-  activeBtn.addEventListener('click', ()=> {selectTer(ter)})
+    // activeBtn.addEventListener('click', ()=> {selectTer(ter)})
   }
 
   fillTerColor(ter)
@@ -108,8 +120,23 @@ function setTextBox() {
   textBox().textContent = `Available troops: ${powerStore}`
 }
 
-// Change background based on active
+// Change active territory
+function selectTerritory(ter) {
+ ter.active = !ter.active
+ fetch(`http://localhost:3000/territories/${ter.id}`, {
+   method: "PATCH",
+   headers: {
+     "Content-Type": "application/json",
+     "Accept": "application/json"
+   },
+   body: JSON.stringify({active: ter.active})
+ })
+ console.log(`territory ${ter.id} active: ${ter.active}`)
+ fillTerColor(ter)
+}
+
 function fillTerColor(ter) {
+  let terDiv = document.querySelector(`#territory-${ter.id}`)
 
   if (ter.active) {
     terDiv.style.backgroundColor = 'purple'
--- src/territory.js
+++ src/territory.js
@@ -1,7 +1,33 @@
-// function createTerritory() {
-//   let allTerritories = []
-//
-//   return class {
-//     constructor()
-//   }
-// }
+function createTerritory() {
+  let allTerritories = []
+
+  return class {
+    constructor(territoryData) {
+      this.id = territoryData.id
+      this.player_id = territoryData.player_id
+      this.power = territoryData.power
+      this.active = territoryData.active
+      this.neighbors = territoryData.neighbors
+      allTerritories.push(this)
+    }
+
+    static all() {
+      return allTerritories
+    }
+
+    static find(id) {
+      return this.all().find(ter => ter.id == id)
+    }
+
+    static createTerritoriesFromDB(data) {
+      data.forEach(ter => new Territory(ter))
+    }
+
+    static anyActiveTerritories() {
+      return !!this.all().find(ter => ter.active == true)
+    }
+
+  }
+}
+
+const Territory = createTerritory()
