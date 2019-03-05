5abe319706a892b7a1135cf510bb64091b092daf
github username <github email address>
2019-03-05 10:26:40 -0500

    added proof of concept for map integration

---
+++ src/test.js
@@ -0,0 +1,32 @@
+
+
+document.getElementById('map-ter-1').addEventListener('click', ter1)
+document.getElementById('map-ter-2').addEventListener('click', ter2)
+document.getElementById('map-ter-3').addEventListener('click', ter3)
+document.getElementById('map-ter-4').addEventListener('click', ter4)
+
+function ter1(e) {
+  e.preventDefault()
+  console.log('you selected ter 1!')
+}
+
+function ter2(e) {
+  e.preventDefault()
+  console.log('you selected ter 2!')
+}
+
+function ter3(e) {
+  e.preventDefault()
+  console.log('you selected ter 3!')
+}
+
+function ter4(e) {
+  e.preventDefault()
+  console.log('you selected ter 4!')
+}
+
+// document.addEventListener('click', where)
+//
+// function where(e) {
+//   console.log(e.pageX, e.pageY)
+// }
